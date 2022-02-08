a4eb3f01267ce0f75f17186036fe0de7cc8abea8
mk360 <hamoudi191@hotmail.fr>
2022-02-08 23:44:05 +0100

    as const

--- 3.const/1.premise.ts
+++ 3.const/1.premise.ts
@@ -2,11 +2,14 @@
 // however, it behaves differently with values (strings, numbers) 
 // and references (arrays, objects, functions)
 
+// these values will never change, and are never allowed to change
+
 const constant = "constant string";
 const pi = 3.14;
 const isTrue = true;
 
-// these values will never change, and are never allowed to change
+
+// although they were initialized with const, you still can edit them
 
 const array = [1, 2, 3, 4];
 
@@ -14,7 +17,6 @@ const object = {
     property1: "value",
 };
 
-// although they were initialized with const, you still can edit them
 
 array.push(36);
 array.unshift(50);
---
+++ 3.const/2.as-const.ts
@@ -0,0 +1,18 @@
+// in order to match const's behavior with values,
+// we can use a special "as const" type assertion.
+
+const W = ["first value", "second value"] as const;
+
+// "as const" forces the array / object to be readonly.
+// the "readonly" applied to the array removes *every*
+// method or way to edit the array (.push, pop, shift, etc.)
+
+// since the array is now readonly, TypeScript can now
+// recognize the individual array elements ("first value", 
+// "second value") instead of a simple "string[]"
+
+for (let element of W) {
+    // your IDE can now confirm "element"
+    // is either "first value" or "second value",
+    // applying more accurate logic on your code
+}
